33708a8eee2503a081f0601f6ed213c0d9c6b9a3
chase.rindlisbacher <cjrjazz20@gmail.com>
2023-01-18 20:13:47 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+// Chase Rindlisbacher Mission 2 Assignment 1/18/23
+namespace Mission2Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            // start game with message
+            Console.WriteLine("Welcome to the dice throwing simulator! How many dice rolls would you like to simulate?");
+            string numRolls = Console.ReadLine();
+            int iNumRolls = Convert.ToInt32(numRolls);
+            int[] rollTotals = new int[11];
+            decimal[] percRolled = new decimal[11];
+            string[] percentsOfRolls = new string[11];
+
+            //Simulate the roll of the dice
+            for (int i = 0; i < iNumRolls; i++)
+            {
+                int roll1 = rnd.Next(6) + 1;
+                int roll2 = rnd.Next(6) + 1;
+                int rollSum = roll1 + roll2;
+                rollTotals[(rollSum - 2)]++;
+            }
+            // post message above results that will be calculated below
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS\nEach \"*\" represents 1 % of the total number of rolls.");
+            Console.WriteLine("Total number of rolls equals " + iNumRolls + ".");
+            // calculate percentages of total rolls to determine number of asterisks to print
+            for (int i = 0; i < 11; i++)
+            {
+                percRolled[i] = rollTotals[i] * 100 / iNumRolls;
+                decimal numPercent = percRolled[i];
+                string starSpangledString = "";
+                // Make a string full of asterisks
+                while ((numPercent - 1) >= 0)
+                {
+                    starSpangledString = starSpangledString + '*';
+                    numPercent--;
+                }
+                percentsOfRolls[i] = starSpangledString;
+                Console.WriteLine((i + 2) + ": " + percentsOfRolls[i]);
+            }
+        }
+    } // end class
+}
