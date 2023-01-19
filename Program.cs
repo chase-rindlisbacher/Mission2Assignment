93fb8b077dbf5bb6fe9a4e15c0c0eea3705bcd69
chase.rindlisbacher <cjrjazz20@gmail.com>
2023-01-18 20:19:17 -0700

    commented it out

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 // Chase Rindlisbacher Mission 2 Assignment 1/18/23
+// make a dice rolling simulation and take the percentages of each number rolled and represent them as asterisks
 namespace Mission2Assignment
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            // will need Random library for dice rolling simulation
             Random rnd = new Random();
             // start game with message
             Console.WriteLine("Welcome to the dice throwing simulator! How many dice rolls would you like to simulate?");
+            // initialize necessary variables and arrays, also grab user input for how many times to roll and convert to int.
             string numRolls = Console.ReadLine();
             int iNumRolls = Convert.ToInt32(numRolls);
             int[] rollTotals = new int[11];
@@ -26,7 +29,7 @@ namespace Mission2Assignment
             // post message above results that will be calculated below
             Console.WriteLine("DICE ROLLING SIMULATION RESULTS\nEach \"*\" represents 1 % of the total number of rolls.");
             Console.WriteLine("Total number of rolls equals " + iNumRolls + ".");
-            // calculate percentages of total rolls to determine number of asterisks to print
+            // calculate percentages of total rolls to determine number of asterisks to print and prints them
             for (int i = 0; i < 11; i++)
             {
                 percRolled[i] = rollTotals[i] * 100 / iNumRolls;
